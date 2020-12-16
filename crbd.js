435cd3319de95dc3432287e016cb2ccd62106028
Johannes Klein <johannes.t.klein@gmail.com>
2020-12-16 13:36:01 +0100

    Update crbd.js

--- crbd.js
+++ crbd.js
@@ -1,19 +1,19 @@
-var tree = phyjs.read_phyjson('bombac.phyjson')
-phyjs.print_tree(tree)
-tree
+var tree = phyjs.read_phyjson('bombac.phyjson');
+phyjs.print_tree(tree);
+tree;
 
 var model = function () {
-  var lambda = exponential({ a: 1 })
-  var epsilon = uniform({ a: 0.0, b: 1.0 })
-  var mu = epsilon * lambda
+  var lambda = exponential({ a: 1 });
+  var epsilon = uniform({ a: 0.0, b: 1.0 });
+  var mu = epsilon * lambda;
 
-  factor(exactCRBDLikelihoodComplete(tree, lambda, mu))
+  factor(exactCRBDLikelihoodComplete(tree, lambda, mu));
 
-  return [lambda, mu]
-}
+  return [lambda, mu];
+};
 
-var dist = Infer({ model, method: 'MCMC', samples: 1000, lag: 10, burn: 1000 })
-display(dist)
+var dist = Infer({ model, method: 'MCMC', samples: 1000, lag: 10, burn: 1000 });
+display(dist);
 
 // Write data:
 json.write('calc.json', dist.getDist())
