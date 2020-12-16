91ea52d29fe2c886bb2ad426c49aaa291917431a
Johannes Klein <johannes.t.klein@gmail.com>
2020-12-16 13:36:25 +0100

    Extract and display values for lambda and mu from the calculation results

--- crbd.js
+++ crbd.js
@@ -15,5 +15,29 @@ var model = function () {
 var dist = Infer({ model, method: 'MCMC', samples: 1000, lag: 10, burn: 1000 });
 display(dist);
 
+var lambdaExtractor = function (x) {
+  return x.value[0];
+};
+var lambdas = map(lambdaExtractor, dist.samples);
+
+var muExtractor = function (x) {
+  return x.value[1];
+};
+var mus = map(muExtractor, dist.samples);
+
+display('lambda computed mean');
+display(listMean(lambdas));
+display('lambda computed variance');
+display(listVar(lambdas));
+display('lambda computed standard deviation');
+display(listStdev(lambdas));
+
+display('mu computed mean');
+display(listMean(mus));
+display('mu computed variance');
+display(listVar(mus));
+display('mu computed standard deviation');
+display(listStdev(mus));
+
 // Write data:
-json.write('calc.json', dist.getDist())
+json.write('calc.json', dist.samples);
